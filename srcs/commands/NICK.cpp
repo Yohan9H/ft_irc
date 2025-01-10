29185d31ea26be47bed6b1f1916d84b35ca2d6f2
Yohan9H <yhurtebize58@gmail.com>
2025-01-10 15:55:36 +0100

    yoyo change arthur

--- srcs/commands/NICK.cpp
+++ srcs/commands/NICK.cpp
@@ -3,17 +3,16 @@
 NICK::NICK() : Command("NICK", 1, false) {};
 NICK::~NICK() {};
 
-void NICK::execCommand(Server &serv, Client &cli, const com &cmd)
+void NICK::execCommand(Server &serv, Client &client, const com &cmd)
 {
-    	std::string msg;
-        std::string nick = cmd.params[0];
+    std::string msg;
+    std::string nick = cmd.params[0];
 
 	// Verif valide name
-
 	if (nick.length() > 9)
 	{
 		msg = msg_err(NAME_SERV, "432", nick, ":Erroneous nickname");
-		send(cli.getClientSocket(), msg.c_str(), msg.size(), 0);
+		send(client.getClientSocket(), msg.c_str(), msg.size(), 0);
 		return ;
 	}
 	bool verif_char = true;
@@ -28,13 +27,13 @@ void NICK::execCommand(Server &serv, Client &cli, const com &cmd)
 	if (verif_char == false)
 	{
 		msg = msg_err(NAME_SERV, "432", nick, ":Erroneous nickname");
-		send(cli.getClientSocket(), msg.c_str(), msg.size(), 0);
+		send(client.getClientSocket(), msg.c_str(), msg.size(), 0);
 		return ;
 	}
 	if (std::isdigit(nick[0]))
 	{
 		msg = msg_err(NAME_SERV, "432", nick, ":Erroneous nickname (debug - isalpha)");
-		send(cli.getClientSocket(), msg.c_str(), msg.size(), 0);
+		send(client.getClientSocket(), msg.c_str(), msg.size(), 0);
 		return ;
 	}
 	
@@ -45,29 +44,24 @@ void NICK::execCommand(Server &serv, Client &cli, const com &cmd)
 	{
 		if (it->second->getNickname() == nick)
 		{
-			std::cout << "in if" << std::endl;
 			msg = msg_err(NAME_SERV, "433", nick, ":Nickname is already in use");
-			send(cli.getClientSocket(), msg.c_str(), msg.size(), 0);
+			send(client.getClientSocket(), msg.c_str(), msg.size(), 0);
 			return ;
 		}
 	}
 
-	cli.setNick(nick);
-
-	// -- Voir si besoin d'envoye un msg a client qui met ou change son nom ?? --
-
 	// Informe tous les channels dont il fait partie de son changement
-	if (cli.getUsername().empty())
+	if (client.getIsAuth() == false)
 	{
-		msg = ":_NoName_!user@" + std::string(NAME_SERV) + "NICK :";
-		cli.sendMsgAllChan(serv, msg); 
+		msg = ":" + nick + " NICK :" + nick ENDLINE_MSG;
+		send(client.getClientSocket(), msg.c_str(), msg.size(), 0);
 	}
 	else
 	{
-		msg = ":" + cli.getUsername() + " !user@" + std::string(NAME_SERV) + "NICK :";
-		cli.sendMsgAllChan(serv, msg);
+		msg = ":" + client.getNickname() + " NICK :" + nick ENDLINE_MSG;
+		client.sendMsgAllChan(serv, msg);
 	}
 
-	cli.print_for_test();
+	client.setNick(nick);
+
 	return ;
-}
