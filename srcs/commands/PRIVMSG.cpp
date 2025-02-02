5326d88174a17d19b985268266e117a5264f33ab
Arthur <arthur.pernot@gmail.com>
2025-02-02 16:28:53 +0100

    fix on for loop privmsg + seg fault when no targetclient

--- srcs/commands/PRIVMSG.cpp
+++ srcs/commands/PRIVMSG.cpp
@@ -5,58 +5,60 @@ PRIVMSG::~PRIVMSG() {};
 
 void PRIVMSG::execCommand(Server &serv, Client &client, const com &cmd)
 {
-
-
-	std::string target = cmd.params[0];
+	std::vector<std::string> targetList = split(cmd.params[0], ',');
 	std::string message = cmd.trailing;
 	std::string msg;
 	int numeric;
+	for (std::vector<std::string>::iterator it = targetList.begin(); it != targetList.end(); it++)
+	{
+		std::string target = *it;
 
-	if (target.empty()) 
-	{
-		msg = "No recipient given " + cmd.command;
-		numeric = ERR_NORECIPIENT;
-		OutDataNumericParam1(client, numeric, client.getNickname(), msg + ENDLINE_MSG);
-	} 
-	else if (!cmd.hasText)
-	{
-		msg = "No text to send";
-		numeric = ERR_NOTEXTTOSEND;
-		OutDataNumericParam1(client, numeric, client.getNickname(), msg + ENDLINE_MSG);
-	} 
-	else if (target[0] == '#')
-	{
-		Channel* channel = serv.getChannelbyName(target);
-		if (!channel)
+		if (target.empty()) 
 		{
-			msg = "No such channel";
-			numeric = ERR_NOSUCHCHANNEL;
-			OutDataNumericParam2(client, numeric, client.getNickname(), target, msg + ENDLINE_MSG);
-		}
-		else if (!channel->checkClientIsMembre(client.getClientSocket()))
+			msg = "No recipient given " + cmd.command;
+			numeric = ERR_NORECIPIENT;
+			OutDataNumericParam1(client, numeric, client.getNickname(), msg + ENDLINE_MSG);
+		} 
+		else if (!cmd.hasText)
 		{
-			msg = "Cannot send to channel";
-			numeric = ERR_CANNOTSENDTOCHAN;
-			OutDataNumericParam2(client, numeric, client.getNickname(), target, msg + ENDLINE_MSG);		}
-		else
+			msg = "No text to send";
+			numeric = ERR_NOTEXTTOSEND;
+			OutDataNumericParam1(client, numeric, client.getNickname(), msg + ENDLINE_MSG);
+		} 
+		else if (target[0] == '#')
 		{
-			std::string privmsg = ":" + client.getNickname() + "!" + client.getUsername() + "@localhost PRIVMSG " + target + " :" + message + ENDLINE_MSG;
-			channel->sendMsgMembresExceptFd(privmsg, client.getClientSocket(), serv);
-		}	
-	} 
-	else 
-	{
-		Client* targetclient = serv.getClientbyName(target);
-		if (!targetclient)
+			Channel* channel = serv.getChannelbyName(target);
+			if (!channel)
+			{
+				msg = "No such channel";
+				numeric = ERR_NOSUCHCHANNEL;
+				OutDataNumericParam2(client, numeric, client.getNickname(), target, msg + ENDLINE_MSG);
+			}
+			else if (!channel->checkClientIsMembre(client.getClientSocket()))
+			{
+				msg = "Cannot send to channel";
+				numeric = ERR_CANNOTSENDTOCHAN;
+				OutDataNumericParam2(client, numeric, client.getNickname(), target, msg + ENDLINE_MSG);		}
+			else
+			{
+				std::string privmsg = ":" + client.getNickname() + "!" + client.getUsername() + "@localhost PRIVMSG " + target + " :" + message + ENDLINE_MSG;
+				channel->sendMsgMembresExceptFd(privmsg, client.getClientSocket(), serv);
+			}	
+		} 
+		else 
 		{
-			msg = "No such nick";
-			numeric = ERR_NOSUCHNICK;
-			OutDataNumericParam2(client, numeric, client.getNickname(), targetclient->getNickname(), msg + ENDLINE_MSG);
+			Client* targetclient = serv.getClientbyName(target);
+			if (!targetclient)
+			{
+				msg = "No such nick";
+				numeric = ERR_NOSUCHNICK;
+				OutDataNumericParam2(client, numeric, client.getNickname(), target, msg + ENDLINE_MSG);
 
-		}
-		else {
-			std :: string privmsg = ":" + client.getNickname() + "!" + client.getUsername() + "@localhost PRIVMSG " + targetclient->getNickname() + " :" + message + ENDLINE_MSG;
-			targetclient->appendOutData(msg);
+			}
+			else {
+				std :: string privmsg = ":" + client.getNickname() + "!" + client.getUsername() + "@localhost PRIVMSG " + target + " :" + message + ENDLINE_MSG;
+				targetclient->appendOutData(privmsg);
+			}
 		}
 	}
 }
